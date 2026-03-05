222dc2e14a8598aae0a957937edeb7ac1450bb58
chenchengpeng <chenchengpeng@richinfo.cn>
2026-03-05 11:22:42 +0800

    feat: 重构文件信息处理逻辑，使用结构体替代映射以增强类型安全性

--- cmd/autoUpdatePic/main.go
+++ cmd/autoUpdatePic/main.go
@@ -17,6 +17,16 @@ const (
 	dateDir           = "202505"
 )
 
+type FileInfo struct {
+	fullPath   string
+	fileName   string
+	nameOnly   string
+	category   string
+	targetDir  string
+	cssPath    string
+	cssContent string
+}
+
 func main() {
 	isHome := os.Getenv("IS_HOME") == "1"
 	basePath := companySourcePath
@@ -36,7 +46,7 @@ func main() {
 	}
 
 	// 按 CSS 路径分组收集处理信息
-	cssGroups := make(map[string][]map[string]string)
+	cssGroups := make(map[string][]*FileInfo)
 	count := 0
 
 	for _, f := range files {
@@ -45,7 +55,7 @@ func main() {
 		}
 		info := prepareFileInfo(basePath, f.Name())
 		if info != nil {
-			cssGroups[info["cssPath"]] = append(cssGroups[info["cssPath"]], info)
+			cssGroups[info.cssPath] = append(cssGroups[info.cssPath], info)
 			count++
 		}
 	}
@@ -58,43 +68,44 @@ func main() {
 	fmt.Printf("[完成] 共处理 %d 个文件。\n", count)
 }
 
-func prepareFileInfo(basePath, fileName string) map[string]string {
+func prepareFileInfo(basePath, fileName string) *FileInfo {
 	fullPath := filepath.Join(compressedPath, fileName)
 	width, height := getImageDimension(fullPath)
 	ext := filepath.Ext(fileName)
 	nameOnly := strings.TrimSuffix(fileName, ext)
 
-	info := make(map[string]string)
-	info["fullPath"] = fullPath
-	info["fileName"] = fileName
-	info["nameOnly"] = nameOnly
+	info := &FileInfo{
+		fullPath: fullPath,
+		fileName: fileName,
+		nameOnly: nameOnly,
+	}
 
 	if width == 220 && height == 220 {
-		info["category"] = "popQy"
-		info["targetDir"] = filepath.Join(basePath, "components/xdrsign/static/popQy")
-		info["cssPath"] = filepath.Join(basePath, "components/xdrsign/index.css")
-		info["cssContent"] = fmt.Sprintf(".level-sign-popup .level-sign-popup-prize.%s {\n    background-image: url('../../components/xdrsign/static/popQy/%s');\n}\n", nameOnly, fileName)
+		info.category = "popQy"
+		info.targetDir = filepath.Join(basePath, "components/xdrsign/static/popQy")
+		info.cssPath = filepath.Join(basePath, "components/xdrsign/index.css")
+		info.cssContent = fmt.Sprintf(".level-sign-popup .level-sign-popup-prize.%s {\n    background-image: url('../../components/xdrsign/static/popQy/%s');\n}\n", nameOnly, fileName)
 	} else if width == 200 && height == 208 {
-		info["category"] = "signPrize"
-		info["targetDir"] = filepath.Join(basePath, "components/xdrsign/static")
-		info["cssPath"] = filepath.Join(basePath, "components/xdrsign/index.css")
-		info["cssContent"] = fmt.Sprintf(".level-sign-prize-wrapper #level-sign-prize-swiper .swiper-slide.%s {\n    background-image: url('../../components/xdrsign/static/%s');\n}\n", nameOnly, fileName)
+		info.category = "signPrize"
+		info.targetDir = filepath.Join(basePath, "components/xdrsign/static")
+		info.cssPath = filepath.Join(basePath, "components/xdrsign/index.css")
+		info.cssContent = fmt.Sprintf(".level-sign-prize-wrapper #level-sign-prize-swiper .swiper-slide.%s {\n    background-image: url('../../components/xdrsign/static/%s');\n}\n", nameOnly, fileName)
 	} else {
-		info["targetDir"] = filepath.Join(basePath, "images/xdrNormal", dateDir)
-		info["cssPath"] = filepath.Join(basePath, "css/xdrNormal.css")
+		info.targetDir = filepath.Join(basePath, "images/xdrNormal", dateDir)
+		info.cssPath = filepath.Join(basePath, "css/xdrNormal.css")
 		if strings.HasSuffix(nameOnly, "_not_start") {
-			info["category"] = "notStart"
+			info.category = "notStart"
 		} else if strings.Contains(nameOnly, "_xdr") {
-			info["category"] = "xdrPrize"
+			info.category = "xdrPrize"
 		} else {
-			info["category"] = "normalPrize"
+			info.category = "normalPrize"
 		}
-		info["cssContent"] = generateNormalCSS(nameOnly, fileName)
+		info.cssContent = generateNormalCSS(nameOnly, fileName)
 	}
 	return info
 }
 
-func updateBatch(cssPath string, infos []map[string]string) {
+func updateBatch(cssPath string, infos []*FileInfo) {
 	data, err := os.ReadFile(cssPath)
 	if err != nil {
 		fmt.Printf("[错误] 读取 CSS 失败 %s: %v\n", cssPath, err)
@@ -112,16 +123,16 @@ func updateBatch(cssPath string, infos []map[string]string) {
 
 	for _, info := range infos {
 		// 1. 移动文件
-		os.MkdirAll(info["targetDir"], 0755)
-		moveFile(info["fullPath"], filepath.Join(info["targetDir"], info["fileName"]))
-		fmt.Printf("[移动] %s -> %s\n", info["fileName"], info["targetDir"])
+		os.MkdirAll(info.targetDir, 0755)
+		moveFile(info.fullPath, filepath.Join(info.targetDir, info.fileName))
+		fmt.Printf("[移动] %s -> %s\n", info.fileName, info.targetDir)
 
 		// 2. 内存中构造新 CSS
-		if strings.Contains(contentStr, info["nameOnly"]) {
+		if strings.Contains(contentStr, info.nameOnly) {
 			continue
 		}
 
-		searchKey := searchKeys[info["category"]]
+		searchKey := searchKeys[info.category]
 		lastIdx := strings.LastIndex(contentStr, searchKey)
 		insertIdx := len(contentStr)
 		if lastIdx != -1 {
@@ -129,7 +140,7 @@ func updateBatch(cssPath string, infos []map[string]string) {
 				insertIdx = lastIdx + endBraceIdx + 1
 			}
 		}
-		contentStr = contentStr[:insertIdx] + "\n" + info["cssContent"] + contentStr[insertIdx:]
+		contentStr = contentStr[:insertIdx] + "\n" + info.cssContent + contentStr[insertIdx:]
 	}
 
 	os.WriteFile(cssPath, []byte(contentStr), 0644)
@@ -161,50 +172,6 @@ func generateNormalCSS(name, file string) string {
 	}
 
 	return fmt.Sprintf("/* %s */\n.level-award-prize .item.%s {\n    background-image: url('../images/xdrNormal/%s/%s');\n}\n", name, name, dateDir, file)
-}
-
-func appendCSS(info map[string]string) error {
-	path := info["cssPath"]
-	content := info["cssContent"]
-	keyword := info["nameOnly"]
-	category := info["category"]
-
-	data, err := os.ReadFile(path)
-	if err != nil {
-		return err
-	}
-	contentStr := string(data)
-
-	if strings.Contains(contentStr, keyword) {
-		fmt.Printf("[跳过] 样式已存在: %s\n", keyword)
-		return nil
-	}
-
-	// 定义各类样式的特征选择器 map
-	searchKeys := map[string]string{
-		"popQy":       ".level-sign-popup .level-sign-popup-prize",
-		"signPrize":   ".level-sign-prize-wrapper #level-sign-prize-swiper .swiper-slide",
-		"notStart":    "#XdrNotStartList #not-start-swiper .swiper-slide",
-		"xdrPrize":    "#XdrPrizeList .level-award-prize .item",
-		"normalPrize": ".level-award-prize .item",
-	}
-
-	searchKey := searchKeys[category]
-	lastIdx := strings.LastIndex(contentStr, searchKey)
-	insertIdx := len(contentStr)
-
-	if lastIdx != -1 {
-		// 寻找该选择器块结束的 '}'
-		afterMatch := contentStr[lastIdx:]
-		endBraceIdx := strings.Index(afterMatch, "}")
-		if endBraceIdx != -1 {
-			insertIdx = lastIdx + endBraceIdx + 1
-		}
-	}
-
-	// 插入内容
-	newContent := contentStr[:insertIdx] + "\n" + content + contentStr[insertIdx:]
-	return os.WriteFile(path, []byte(newContent), 0644)
 }
 
 func getImageDimension(path string) (int, int) {
